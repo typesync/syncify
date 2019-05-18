ad38f4b6ec1846c989d4aba5b1ea841dca61247a
Eloi Simard-Quesnel <eloi.qs@outlook.com>
2019-05-18 14:22:37 -0400

    ... socket io prototype

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 import { LogLevels } from 'shared';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    io(`http://localhost:${process.env.REACT_APP_SERVER_PORT}`);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,9 +16,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p data-testid="log-level">
-          {LogLevels.Debug}
-        </p>
+        <p data-testid="log-level">{LogLevels.Debug}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -25,6 +28,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,17 +1,23 @@
 import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as http from 'http';
 import { LogLevels } from 'shared';
+import * as socketIo from 'socket.io';
 import { log } from '../logger';
 
 export function bootstrap() {
   dotenv.config();
 
   const app = express();
-  const port = process.env.PORT;
+  const server = new http.Server(app);
 
-  app.use(express.static(`${__dirname}/../../../client/build`));
+  const io = socketIo(server);
+  io.on('connection', socket => {
+    log({ level: LogLevels.Debug, message: 'A user connected' });
+  });
 
-  app.listen(port, () =>
+  const port = process.env.SERVER_PORT;
+  server.listen(port, () =>
     log({
       level: LogLevels.Debug,
       message: `Example app listening on port ${port}!`,
